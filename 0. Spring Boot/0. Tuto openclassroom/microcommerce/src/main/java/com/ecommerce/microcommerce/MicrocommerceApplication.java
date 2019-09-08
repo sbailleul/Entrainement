16752bf425dfa77305ae9112755fc457dcd3d43c
Bailleul <bailleul.sacha@gmail.com>
2019-09-08 19:18:07 +0200

    modif arborescence

--- 0. Spring Boot/0. Tuto openclassroom/microcommerce/src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
+++ 0. Spring Boot/0. Tuto openclassroom/microcommerce/src/main/java/com/ecommerce/microcommerce/MicrocommerceApplication.java
@@ -2,12 +2,14 @@ package com.ecommerce.microcommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class MicrocommerceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicrocommerceApplication.class, args);
+	    SpringApplication.run(MicrocommerceApplication.class, args);
 	}
 
 }
